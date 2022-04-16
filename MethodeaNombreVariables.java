0b8312d28a30c869341f65007a4c0cbd726aea4f
hayet-dev <64528762+hayet-dev@users.noreply.github.com>
2022-04-16 23:20:09 +0200

    Add files via upload

---
+++ MethodeaNombreVariables.java
@@ -0,0 +1,43 @@
+
+public class MethodeaNombreVariables {
+
+	
+	public static int add(int value []) {
+		int accumulator =0;
+				for (int val : value) {
+					accumulator+=val;		
+				}
+	
+		return accumulator;
+	}
+	
+	
+	
+	
+	
+		//public static void main(String[] args) {
+//	
+//			int []value= {10,20,30,44};
+//		
+//		System.out.println(add(value));
+//		
+//		
+		
+	//}
+public static void main(String... args) {
+	
+//int []value= {10,20,30,44};
+//System.out.println(add(value));
+
+	
+System.out.println(add(new int[] {10,20,30,40}));//meilleur methode ADD additionner 
+
+
+
+
+//}
+	
+
+//}
+}
+}
